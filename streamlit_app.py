558300f4beb6e3b98638b4a6114a49b58b6df77d
DataGoo <156483382+DataGoo@users.noreply.github.com>
2024-05-08 23:05:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,13 @@ from ydata_profiling import ProfileReport
 import streamlit.components.v1 as components
 from streamlit_pandas_profiling import st_profile_report
 import sweetviz as sv
-
+import plotly.express as px
 
 # Configurar Streamlit para usar todo el ancho de la pantalla
 st.set_page_config(layout="wide")
 
-# Cargar el DataFrame
+# Decorador para cachear la carga del DataFrame y evitar recargas innecesarias
+@st.cache_data
 def cargar_dataframe(archivo):
     try:
         df = pd.read_csv(archivo)
@@ -20,72 +21,70 @@ def cargar_dataframe(archivo):
         st.error('El archivo CSV está vacío o no tiene columnas. Por favor, carga un archivo válido.')
         return None
 
-# Crear pestañas
-titulos_pestanas = ['Carga de Datos', 'Estadísticas Básicas', 'Análisis EDA', 'Análisis Avanzado', 'Gráficos', 'Acerca de']
-pestaña_carga, pestaña_basicas, pestaña_eda, pestaña_avanzado, pestaña_graficos, pestaña_about = st.tabs(titulos_pestanas)
+def main():
+    # Crear pestañas
+    titulos_pestanas = ['Carga de Datos', 'Estadísticas Básicas', 'Análisis EDA', 'Análisis Avanzado', 'Gráficos', 'Acerca de']
+    pestañas = st.tabs(titulos_pestanas)
 
-# Agregar contenido a cada pestaña
-with pestaña_carga:
-    st.header('Carga de Datos')
-    archivo_cargado = st.file_uploader("Elige un archivo CSV", type=['csv'])
-    if archivo_cargado is not None:
-        df = cargar_dataframe(archivo_cargado)
-        if df is not None:
-            st.write(df)
+    # Inicialización de variables en el estado de sesión
+    if 'df_cargado' not in st.session_state:
+        st.session_state.df_cargado = None
 
-with pestaña_basicas:
-    st.header('Estadísticas Básicas')
-    if archivo_cargado is not None and df is not None:
-        st.write('Descripción del DataFrame:')
-        st.write(df.describe())
-        st.write(df.describe(include='object'))
+    # Agregar contenido a la pestaña de Carga de Datos
+    with pestañas[0]:
+        st.header('Carga de Datos')
+        archivo_cargado = st.file_uploader("Elige un archivo CSV", type=['csv'])
+        if archivo_cargado is not None:
+            df = cargar_dataframe(archivo_cargado)
+            if df is not None:
+                st.session_state.df_cargado = df
+                st.write(df)
 
-with pestaña_eda:
-    st.header('Análisis Exploratorio de Datos (EDA)')
-    st.write('Aquí puedes agregar el contenido de análisis EDA')
-    if archivo_cargado is not None:
+    df = st.session_state.df_cargado
 
-            if st.button("Generate Sweetviz Report"):
-                report = sv.analyze(df)
-                # Directamente especificamos la ruta del archivo donde se guardará el reporte
-                report_file = 'SWEETVIZ_REPORT1.html'
-                # Guardamos el reporte en la ruta especificada
-                report.show_html(filepath=report_file, open_browser=False)
-                # Usamos un bloque with para asegurarnos de que el archivo se cierre correctamente después de abrirlo
-                with open(report_file, 'r', encoding='utf-8') as HtmlFile:
-                    source_code = HtmlFile.read()
-                    components.html(source_code, height=800, width=1080, scrolling=True)        # Aquí puedes agregar el contenido de análisis EDA
-                 
-with pestaña_avanzado:
-    st.header('Análisis Avanzado')
-    if archivo_cargado is not None and df is not None:
-        profile = ProfileReport(df, explorative=True)
-        st_profile_report(profile)
+    if df is not None:
+        # Pestaña de Estadísticas Básicas
+        with pestañas[1]:
+            st.header('Estadísticas Básicas')
+            st.write('Descripción del DataFrame:')
+            st.write(df.describe())
+            st.write(df.describe(include='object'))
 
-with pestaña_graficos:
-    st.header('Gráficos')
-    if archivo_cargado is not None and df is not None:
-        # Permitir al usuario seleccionar hasta 6 columnas para graficar
-        columnas_seleccionadas = st.multiselect(
-            'Selecciona hasta 6 columnas para graficar:',
-            df.columns,
-            default=df.columns[:min(6, len(df.columns))].tolist()  # Convertir a lista
-        )
-        # Crear contenedores para 2 columnas
-        col1, col2 = st.columns(2)
-        for i, columna in enumerate(columnas_seleccionadas[:6]):  # Limitar a 6 gráficos
-            with col1 if i % 2 == 0 else col2:  # Alternar entre las dos columnas
-                if df[columna].dtype == 'object':  # Datos categóricos
-                    st.subheader(f'Gráfico de Barras para {columna}')
-                    fig, ax = plt.subplots()
-                    df[columna].value_counts().plot(kind='bar', ax=ax)
-                    st.pyplot(fig)
-                else:  # Datos numéricos
-                    st.subheader(f'Gráfico de Distribución para {columna}')
-                    fig, ax = plt.subplots()
-                    sns.histplot(df[columna], kde=True, ax=ax)
-                    st.pyplot(fig)
+        # Pestaña de Análisis EDA
+        with pestañas[2]:
+            st.header('Análisis Exploratorio de Datos (EDA)')
+            st.write('Aquí puedes agregar el contenido de análisis EDA')
 
-with pestaña_about:
-    st.header('Acerca de')
-    st.write('Esta es una aplicación te permite navegar entre pestañas y ver análisis de los datos.')
+        # Pestaña de Análisis Avanzado
+        with pestañas[3]:
+            st.header('Análisis Avanzado')
+            profile = ProfileReport(df, explorative=True)
+            st_profile_report(profile)
+
+        # Pestaña de Gráficos
+        with pestañas[4]:
+            st.header('Gráficos')
+            columnas_seleccionadas = st.multiselect(
+                'Selecciona hasta 6 columnas para graficar:',
+                df.columns,
+                default=df.columns[:min(6, len(df.columns))].tolist()
+            )
+            col1, col2 = st.columns(2)
+            for i, columna in enumerate(columnas_seleccionadas[:6]):
+                with col1 if i % 2 == 0 else col2:
+                    if df[columna].dtype == 'object':
+                        st.subheader(f'Gráfico de Barras para {columna}')
+                        fig = px.bar(df, x=columna, title=f'Gráfico de Barras para {columna}')
+                        st.plotly_chart(fig)
+                    else:
+                        st.subheader(f'Gráfico de Distribución para {columna}')
+                        fig = px.histogram(df, x=columna, marginal='rug', title=f'Gráfico de Distribución para {columna}')
+                        st.plotly_chart(fig)
+
+    # Pestaña Acerca de
+    with pestañas[5]:
+        st.header('Acerca de')
+        st.write('Esta es una aplicación que te permite navegar entre pestañas y ver análisis de los datos.')
+
+if __name__ == "__main__":
+    main()
